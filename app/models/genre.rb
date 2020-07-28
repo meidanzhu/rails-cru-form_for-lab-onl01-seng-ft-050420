a530835f94fd5768fea8d8b2db8feac39bbe454d
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-07-28 21:35:26 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,3 +1,7 @@
 class Genre < ApplicationRecord
+<<<<<<< HEAD
     has_many :songs
+=======
+  has_many :songs
+>>>>>>> 1be8c2498d5e0e37451cb96b9803bc0726058289
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,9 @@
 class Song < ApplicationRecord
+<<<<<<< HEAD
     belongs_to :artist
     belongs_to :genre
+=======
+  belongs_to :artist
+  belongs_to :genre
+>>>>>>> 1be8c2498d5e0e37451cb96b9803bc0726058289
 end
